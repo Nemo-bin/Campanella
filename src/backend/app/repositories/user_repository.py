9a8102403514fe3850ca7194613f37cb18d7708f
Nemo-bin <ben.speeder@gmail.com>
2026-03-05 04:39:34 +0000

    user model, create_user, password hashing

--- src/backend/app/repositories/user_repository.py
+++ src/backend/app/repositories/user_repository.py
@@ -1,23 +1,27 @@
+from sqlalchemy.exc import IntegrityError
 from app.infrastructure.db.models.user_model import UserModel
+from app.utils.security import verify_password
 
 class UserRepository:
     def __init__(self, db):
         self.db = db
 
-    def create_user(self, email, password_hash):
+    def create_user(self, email: str, password_hash: str, username: str) -> UserModel:
         user = UserModel(
             email=email,
-            password_hash=password_hash
+            password_hash=password_hash,
+            username=username,
+            
         )
 
-        self.db.add(user)
-        self.db.commit()
-
-        return user
+        try:
+            self.db.add(user)
+            self.db.commit()
+            self.db.refresh(user)
+            return user
+        except IntegrityError:
+            self.db.rollback()
+            raise ValueError("User with this email or username already exists")
     
     def get_user_by_email(self, email):
-        return (
-            self.db.query(UserModel)
-            .filter(UserModel.email == email)
-            .first()
-        )
+        return self.db.query(UserModel).filter(UserModel.email == email).first()
