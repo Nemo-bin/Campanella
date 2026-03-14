666d08c0c372375e43194c3a56f092df6cc374ac
Ben French <ben.speeder@gmail.com>
2026-03-14 21:29:27 +0000

    Middleware created

--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask, g
 from flask_cors import CORS
+import os
 from app.infrastructure.db.session import SessionLocal
 from app.infrastructure.db import init_db
+from backend.app.middleware.jwt_token import AuthMiddleware
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
+
+    jwt_secret = os.environ.get("JWT_SECRET")
+    if not jwt_secret:
+        raise RuntimeError("JWT_SECRET not set")
+    AuthMiddleware.init(jwt_secret)
 
     init_db()
 
---
+++ src/backend/app/middleware/jwt_token.py
@@ -0,0 +1,53 @@
+import jwt
+from functools import wraps
+from flask import request
+import datetime
+import os
+
+class AuthMiddleware:
+    _secret = None
+
+    @classmethod
+    def init(cls, secret_key: str):
+        cls._secret = secret_key
+
+    @classmethod
+    def required(cls, f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            if cls._secret is None:
+                raise Exception("No JWT key")
+
+            token = None
+            auth_header = request.headers.get("Authorization")
+            if auth_header and auth_header.startswith("Bearer "):
+                token = auth_header.split(" ")[1]
+
+            if not token:
+                raise Exception("Token missng")
+
+            try:
+                data = jwt.decode(token, cls._secret, algorithms=["HS256"])
+                current_user_id = data.get("user_id")
+                if not current_user_id:
+                    raise Exception("Token does not contain user_id")
+
+            except jwt.ExpiredSignatureError:
+                raise Exception("Token expired")
+            except (jwt.InvalidAlgorithmError, jwt.DecodeError):
+                raise Exception("Invalid token")
+
+            return f(current_user_id, *args, **kwargs)
+
+        return decorated
+    
+    @classmethod
+    def create_jwt(cls, user_id: int, expires_hours: int):
+        data = {
+            "user_id": user_id,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=expires_hours),
+            "iat": datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(data, cls._secret, algorithm="HS256")
+        return token
